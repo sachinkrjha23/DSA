86332f44dd79ac2e72548b3c3116cf88c8130c4a
Sachin Kumar Jha <skrjha002@gmail.com>
2025-06-18 11:23:34 +0530

    Revised and made changes accordingly in L-45

--- L-45 Pointers/lecture_1.cpp
+++ L-45 Pointers/lecture_1.cpp
@@ -1,17 +1,35 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-
-int main(){
+int main()
+{
 
     int a = 12;
-    int *ptr = &a;  
-    cout << ptr << endl;  // Prnts address of a
-    cout << *ptr << endl;  // Prints value stored in address
 
-    int b = 10;
-    ptr = &b;  
+    // Print the address of 'a'
+
+    cout << &a << endl;
+    int *ptr = &a;
     cout << ptr << endl;
+
+    // Print the value inside 'a'
+
     cout << *ptr << endl;
 
+    // Change the element inside pointer
+
+    int b = 20;
+    cout << &b << endl;
+    ptr = &b;  // Once pointer is declared no need to declare data type and '*' again
+    cout << ptr << endl;
+
+    // Print the value inside 'b'
+
+    cout << *ptr << endl;
+
+    // Value after updatng 'b'
+
+    b = 35;
+    cout << ptr << endl; // Address doesn't change
+    cout << *ptr << endl;
 }
