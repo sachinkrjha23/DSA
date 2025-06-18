30f050cd05fcacdf37407fb300b8c0a4f1a733bd
Sachin Kumar Jha <skrjha002@gmail.com>
2025-06-18 12:55:31 +0530

    Update L-46

--- L-46 rltn with array/implementation.cpp
+++ L-46 rltn with array/implementation.cpp
@@ -4,21 +4,24 @@ using namespace std;
 int main()
 {
     int arr[5] = {1, 2, 3, 4, 5};
-
-    // Print the address of first element
-    cout << arr << endl;
-    cout << &arr[0] << endl;
     int *ptr = arr;
-    cout << ptr << endl;
 
-    // Print the address of second element
-    cout << arr + 1 << endl;
-    cout << &arr[1] << endl;
+    // // Print the address of first element
+    // cout << arr << endl;
+    // cout << arr+0 << endl;
+    // cout << &arr[0] << endl;
+    // cout << ptr << endl;
 
-    // Print the value of an index
-    cout << arr[0] << endl;
-    cout << *arr << endl;
-    cout << *ptr << endl;
+    // // Print the address of second element
+    // cout << arr + 1 << endl;
+    // cout << &arr[1] << endl;
+    // cout << ptr + 1 << endl;
+
+    // // Print the value of an index
+    // cout << arr[0] << endl;
+    // cout << *arr << endl;
+    // cout << *(arr+0) << endl;
+    // cout << *ptr << endl;
 
     // Print the address of elements
     for (int i = 0; i < 5; i++)
@@ -30,6 +33,25 @@ int main()
     // Print the value of indexes
     for (int i = 0; i < 5; i++)
     {
-        cout << *(arr + i) << endl;
+        cout << *(arr + i) << " ";
     }
+    cout << endl;
+
+    // Arithmetic Operations ptr++, ptr--
+
+    // Print all values
+    for (int i = 0; i < 5; i++)
+    {
+        cout << *ptr << " ";
+        ptr++;
+    }
+    cout << endl;
+
+    ptr = arr+4;  // Move ptr to the last element as its outside due to prev loop
+    for (int i = 4; i >= 0; i--)
+    {
+        cout << *ptr << " ";
+        ptr--;
+    }
+    cout << endl;
 }
