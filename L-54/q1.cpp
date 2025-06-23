304cbf13c07c2e6c316f12e0a1e90a564ccfbd2b
Sachin Kumar Jha <skrjha002@gmail.com>
2025-06-23 17:36:32 +0530

    Update q1.cpp

--- L-54/q1.cpp
+++ L-54/q1.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int fibonacci(int n)
 {
-    if (n == 0)
+    if (n == 1)
         return 0;
-    else if (n == 1)
+    else if (n == 2)
         return 1;
     else
         return fibonacci(n - 1) + fibonacci(n - 2);
