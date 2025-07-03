24a3b9653f31a91129dc1c3ba1933c3fd304ff3e
Sachin Kumar Jha <skrjha002@gmail.com>
2025-07-03 17:22:56 +0530

    Upadte L-64

--- L-64/q1.cpp
+++ L-64/q1.cpp
@@ -3,10 +3,8 @@
 #include <algorithm>
 using namespace std;
 
-// Recursive function to generate all permutations of the array
 void permute(vector<int>& nums, int l, int r) {
 
-    // Base Case: If left index equals right index, we have a complete permutation
     if (l == r) {
         
         // Print the current permutation
@@ -17,7 +15,6 @@ void permute(vector<int>& nums, int l, int r) {
         return;
     }
 
-    // Loop to swap each element with the current index 'l' and recurse
     for (int i = l; i <= r; i++) {
 
         swap(nums[l], nums[i]);       // Step 1: Fix one element at position 'l'
@@ -32,17 +29,16 @@ int main() {
 
     int n;
     cout << "Enter number of elements: ";
-    cin >> n;   // Taking size of the array as input
-
-    vector<int> nums(n);  // Creating a vector to store 'n' elements
+    cin >> n;  
+    vector<int> nums(n);  
 
     cout << "Enter the elements:\n";
     for (int i = 0; i < n; i++) {
-        cin >> nums[i];   // Taking array elements as input
+        cin >> nums[i];   
     }
 
     cout << "All permutations of the array:\n";
-    permute(nums, 0, n - 1);  // Calling the permutation function
+    permute(nums, 0, n - 1);  
 
     return 0;
 }
